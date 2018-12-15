2a78c7cc5ab7062c1deab9280e1bce98962b7b7e
rebecagalante <galante.rebeca@gmail.com>
2018-12-15 12:31:17 -0200

    Novo projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReabilitareFisioterapia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
