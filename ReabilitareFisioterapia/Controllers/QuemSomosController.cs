6f1a541178f4a84f8fb839a49c2720dad53966c7
rebecagalante <galante.rebeca@gmail.com>
2019-01-03 14:09:13 -0200

    Estrutura base do site

--- ReabilitareFisioterapia/Controllers/QuemSomosController.cs
+++ ReabilitareFisioterapia/Controllers/QuemSomosController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace ReabilitareFisioterapia.Controllers
 {
-    public class SobreController : Controller
+    public class QuemSomosController : Controller
     {
         // GET: Sobre
         public ActionResult Index()
